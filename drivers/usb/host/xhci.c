c813346c77e0db1932b9608e1c08dc09d165c1af
Arian <arian.kulmer@web.de>
2022-01-12 18:27:29 +0000

    usb: Import Xiaomi changes
    Change-Id: I3d2f755729bfc5dc8d6c88d58ededdf93db0adfb

--- drivers/usb/host/xhci.c
+++ drivers/usb/host/xhci.c
@@ -1138,6 +1138,9 @@ int xhci_resume(struct xhci_hcd *xhci, bool hibernated)
 	bool			pending_portevent = false;
 	bool			reinit_xhc = false;
 
+	if (!hcd)
+		return 0;
+
 	if (!hcd->state)
 		return 0;
 
